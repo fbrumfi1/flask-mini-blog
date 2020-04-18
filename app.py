e05def96ecb0f6cba642b45112802bad607d6234
fbrumfi1 <59750756+fbrumfi1@users.noreply.github.com>
2020-04-17 20:41:19 -0400

    Added a link for home so the tab could work

--- app.py
+++ app.py
@@ -3,15 +3,19 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def index():
-   return render_template('home.html')
+    return render_template('home.html')
+    
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+@app.route('/base')
+def base():
+    return render_template('base.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
